495b37676f5888c8a2c324ef31be279688d3e68a
Marlon Villarama <marlonsvillarama@gmail.com>
2025-08-26 20:16:43 +1200

    Init shadcn

---
+++ src/components/FieldSet.svelte
@@ -0,0 +1,7 @@
+<script></script>
+
+<div class="field-set">
+    <label for=""></label>
+</div>
+
+<style></style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+    import * as Dialog from "$lib/components/ui/dialog/index.js";
+    import Button, { buttonVariants } from "$lib/components/ui/button/button.svelte";
+    import { Action } from "$lib/components/ui/alert-dialog/index.js";
+    let { data } = $props();
+
+    const clickButton = () => {
+        alert('clicked');
+    };
+</script>
+
+<ul>
+    {#each data.instruments as instrument}
+        <li>{instrument.name}</li>
+    {/each}
+</ul>
+
+<Button variant="default" class="hover:cursor-pointer" onclick={clickButton}>Click me</Button>
+
+<Dialog.Root>
+    <Dialog.Trigger class={[ "cursor-pointer", buttonVariants({ variant: "ghost" }) ]}>Delete Profile</Dialog.Trigger>
+    <Dialog.Content>
+        <Dialog.Header>
+            <Dialog.Title>Are you sure?</Dialog.Title>
+            <Dialog.Description>Deleting your profile cannot be undone. Please consult your Administrator for help.</Dialog.Description>
+        </Dialog.Header>
+        <Dialog.Footer>
+            <Button class="cursor-pointer" variant="default">Proceed</Button>
+            <Button class="cursor-pointer" variant="ghost">Cancel</Button>
+        </Dialog.Footer>
+    </Dialog.Content>
+</Dialog.Root>
